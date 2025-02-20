01da2beb9cf96ccaa25144e9d3e2ad746fedb14a
Njood <nejood03@gmail.com>
2025-02-20 16:05:40 +0300

    step 6 is done

--- src/MusicMaster/songs.kt
+++ src/MusicMaster/songs.kt
@@ -2,9 +2,13 @@ package MusicMaster
 
 fun main() {
     val songs: MutableList<String> = mutableListOf("Euphoria", "Snooze", "Luther")
+
     addSong(songs, "In my room")
     shuffleSongs(songs)
+    removeSong(songs, "Euphoria")
     playAll(songs)
+    val artistSongs = findByArtist(songs, "Luther")
+    val playlist = showPlaylist(songs)
 }
 
 fun playAll(songs: List<String>) {
@@ -12,13 +16,23 @@ fun playAll(songs: List<String>) {
         println("Now playing: $song")
     }
 }
-
 fun addSong(songs: MutableList<String>, songTitle: String) {
     songs.add(songTitle)
-
 }
 
 fun shuffleSongs(songs: MutableList<String>) {
     songs.shuffle()
 }
 
+fun removeSong(songs: MutableList<String>, songTitle: String) {
+    songs.remove(songTitle)
+}
+
+fun findByArtist(songs: List<String>, artistName: String): List<String> {
+    return songs.filter { it.contains(artistName, ignoreCase = true) }
+}
+
+fun showPlaylist(songs: List<String>) {
+    println ("Playlist:")
+    songs.forEach { println("- $it") }
+}
