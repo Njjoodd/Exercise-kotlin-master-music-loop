a85932fa9659262d5b17e0a74d58d1673ca2f331
Njood <nejood03@gmail.com>
2025-02-20 15:46:00 +0300

    step 3 is complete

--- src/MusicMaster/songs.kt
+++ src/MusicMaster/songs.kt
@@ -1,6 +1,11 @@
 package MusicMaster
 fun main () {
     val songs: List<String> = listOf("Euphoria", "Snooze", "Luther",)
-
+    playAll(songs)
 }
 
+fun playAll(songs: List<String>) {
+    for (song in songs) {
+        println("Now playing: $song")
+    }
+}
